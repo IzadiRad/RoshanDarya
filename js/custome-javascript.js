acc5b340da1cf5d4abbe20540258eb0102de496c
Amir Mohammad Izadi Rad <165994224+IzadiRad@users.noreply.github.com>
2024-09-28 16:59:54 +0330

    menuBar done -desktop view need Fix MenuBar -Mobile View Need Hamberger more then this view

--- js/custome-javascript.js
+++ js/custome-javascript.js
@@ -1,27 +1,21 @@
-var swiper = new Swiper(`[unique-script-id="w-w-dm-id"] .mySwiper`, {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-        el: `[unique-script-id="w-w-dm-id"] .swiper-pagination`,
-        clickable: true,
-    },
-    breakpoints: {
+// اسکریپت مربوط به منوی همبرگری
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
 
-        200: {
-            slidesPerView: 1
-        },
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
 
-        501: {
-            slidesPerView: 1.5
-        },
-
-        769: {
-            slidesPerView: 2.5,
-            spaceBetween: 10
-        },
-        1025: {
-            slidesPerView: 3,
-            spaceBetween: 20
-        },
+// اسکریپت مربوط به تغییر رنگ هدر هنگام اسکرول
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('main-header');
+    if (window.scrollY > 100) {
+        header.classList.add('shadow-lg');
+    } else {
+        header.classList.remove('shadow-lg');
     }
 });
+
+
+// WOW.js initialization
+new WOW().init();
