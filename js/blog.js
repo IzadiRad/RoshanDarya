865b09c3927a0a12e3d749cb9eb25f0f6ce001f4
Amir Mohammad Izadi Rad <165994224+IzadiRad@users.noreply.github.com>
2024-10-26 16:37:17 +0330

    fix some issue and add gallery Main Section

--- js/blog.js
+++ js/blog.js
@@ -1,39 +1,36 @@
+document.querySelectorAll('.blogScroll').forEach((scrollContainer) => {
+    let isDown = false;
+    let startX, scrollLeft;
 
-// اضافه کردن قابلیت کشیدن به سمت راست و چپ
-const blogScroll = document.getElementById('blogScroll');
+    scrollContainer.addEventListener('mousedown', (e) => {
+        isDown = true;
+        scrollContainer.classList.add('active');
+        startX = e.pageX - scrollContainer.offsetLeft;
+        scrollLeft = scrollContainer.scrollLeft;
+    });
 
-let isDown = false;
-let startX;
-let scrollLeft;
+    scrollContainer.addEventListener('mouseleave', () => {
+        isDown = false;
+        scrollContainer.classList.remove('active');
+    });
 
-blogScroll.addEventListener('mousedown', (e) => {
-    isDown = true;
-    blogScroll.classList.add('active');
-    startX = e.pageX - blogScroll.offsetLeft;
-    scrollLeft = blogScroll.scrollLeft;
-});
+    scrollContainer.addEventListener('mouseup', () => {
+        isDown = false;
+        scrollContainer.classList.remove('active');
+    });
 
-blogScroll.addEventListener('mouseleave', () => {
-    isDown = false;
-    blogScroll.classList.remove('active');
-});
-
-blogScroll.addEventListener('mouseup', () => {
-    isDown = false;
-    blogScroll.classList.remove('active');
-});
-
-blogScroll.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - blogScroll.offsetLeft;
-    const walk = (x - startX) * 2; // مقدار حرکت ماوس
-    blogScroll.scrollLeft = scrollLeft - walk;
-});
-
-// جلوگیری از باز شدن لینک در حین کشیدن
-blogScroll.addEventListener('click', (e) => {
-    if (isDown) {
+    scrollContainer.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
         e.preventDefault();
-    }
+        const x = e.pageX - scrollContainer.offsetLeft;
+        const walk = (x - startX) * 2; // میزان حرکت
+        scrollContainer.scrollLeft = scrollLeft - walk;
+    });
+
+    // جلوگیری از باز شدن لینک در حین کشیدن
+    scrollContainer.addEventListener('click', (e) => {
+        if (isDown) {
+            e.preventDefault();
+        }
+    });
 });
