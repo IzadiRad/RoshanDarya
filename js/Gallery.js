f680fec1c75d276b64218bc0264918b40e2b53db
Amir Mohammad Izadi Rad <165994224+IzadiRad@users.noreply.github.com>
2024-10-27 14:06:22 +0330

    fix gallery only need to push picture in it

--- js/Gallery.js
+++ js/Gallery.js
@@ -55,3 +55,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
     animateGallery();
 });
+
+
+document.querySelectorAll('.gallery-scroll').forEach((scrollContainer) => {
+    let isDown = false;
+    let startX, scrollLeft;
+
+    scrollContainer.addEventListener('mousedown', (e) => {
+        isDown = true;
+        scrollContainer.classList.add('active');
+        startX = e.pageX - scrollContainer.offsetLeft;
+        scrollLeft = scrollContainer.scrollLeft;
+    });
+
+    scrollContainer.addEventListener('mouseleave', () => {
+        isDown = false;
+        scrollContainer.classList.remove('active');
+    });
+
+    scrollContainer.addEventListener('mouseup', () => {
+        isDown = false;
+        scrollContainer.classList.remove('active');
+    });
+
+    scrollContainer.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - scrollContainer.offsetLeft;
+        const walk = (x - startX) * 2; // میزان حرکت
+        scrollContainer.scrollLeft = scrollLeft - walk;
+    });
+
+    // جلوگیری از باز شدن لینک در حین کشیدن
+    scrollContainer.addEventListener('click', (e) => {
+        if (isDown) {
+            e.preventDefault();
+        }
+    });
+});
