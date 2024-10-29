751b86f44fde09a9202ecbefebb66802eea267d7
Amir Mohammad Izadi Rad <165994224+IzadiRad@users.noreply.github.com>
2024-10-29 13:51:02 +0330

    fix and added some item

--- js/toggleFAQ.js
+++ js/toggleFAQ.js
@@ -1,10 +1,8 @@
 function toggleFAQ(element) {
     const answer = element.nextElementSibling;
-    const icon = element.querySelector('i');
+    const icon = element.querySelector("i");
 
-    // Toggle the answer display
-    answer.classList.toggle('hidden');
-    // Change the icon direction
-    icon.classList.toggle('fa-chevron-down');
-    icon.classList.toggle('fa-chevron-up');
+    answer.classList.toggle("show");
+    icon.classList.toggle("fa-chevron-down");
+    icon.classList.toggle("fa-chevron-up");
 }
